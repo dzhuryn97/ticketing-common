f529628f5b50c32c59131858b4294b65e8f1035c
Volodymyr Dzhuryn <volodymyr.dzhuryn@otakoyi.com>
2024-09-14 19:17:33 +0300

    fix error

--- tests/Infrastructure/Command/OriginalDomainExceptionMiddlewareTest.php
+++ tests/Infrastructure/Command/OriginalDomainExceptionMiddlewareTest.php
@@ -65,10 +65,6 @@ class OriginalDomainExceptionMiddlewareTest extends TestCase
      */
     public function Handle_NextReturnEnvelope_ReturnThoseEnvelope()
     {
-        ()
-
-
-
         //Arrange
         $requestStackWithMainRequestMock = $this->getRequestStackWithMainRequest();
         $originalDomainExceptionMiddleware = new DomainExceptionExtractingMiddleware($requestStackWithMainRequestMock);
